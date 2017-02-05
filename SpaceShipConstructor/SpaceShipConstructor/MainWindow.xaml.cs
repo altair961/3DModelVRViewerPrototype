83d8470d0a6fd813217149164e98a9ad5d474eb1
Renat <Renat@IDEA-PC>
2017-02-05 22:17:16 +0100

    Enabled to show a model in VR style on two viewports

--- SpaceShipConstructor/SpaceShipConstructor/MainWindow.xaml.cs
+++ SpaceShipConstructor/SpaceShipConstructor/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HelixToolkit.Wpf;
+using System.Windows.Media.Media3D;
 
 namespace SpaceShipConstructor
 {
@@ -20,9 +22,39 @@ namespace SpaceShipConstructor
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Model3DGroup Model3DGroup;
+        public ModelImporter ModelImporter;
+        public Model3D SpaceshipModel;
+
+        public Model3D Model { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+
+            ModelImporter = new ModelImporter();
+            Model3DGroup = new Model3DGroup();
+
+            Material material = new DiffuseMaterial(new SolidColorBrush(Colors.Beige));
+            ModelImporter.DefaultMaterial = material;
+
+            SpaceshipModel = ModelImporter.Load(@"Spaceship.obj");
+
+            Model3DGroup.Children.Add(SpaceshipModel);
+
+            this.Model = Model3DGroup;
+            overall_grid.DataContext = this;
+
+            Transform3DGroup transfomrs = new Transform3DGroup();
+
+            RotateTransform3D xRotateTransform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(1, 0, 0), 90));
+            transfomrs.Children.Add(xRotateTransform);
+
+            RotateTransform3D yRotateTransform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 1, 0), 0));
+            transfomrs.Children.Add(yRotateTransform);
+
+            RotateTransform3D zRotateTransform = new RotateTransform3D(new AxisAngleRotation3D(new Vector3D(0, 0, 1), 270));
+            transfomrs.Children.Add(zRotateTransform);
+            Model3DGroup.Transform = transfomrs;
         }
     }
 }
